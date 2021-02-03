2a50634d5ae95139f56d09ac604ca4d84d3fa929
Yudi Prayoga Raharja <yudiprayogaraharja@gmail.com>
2021-02-03 13:28:34 +0700

    add checkongkir controller

---
+++ app/Http/Controllers/ShippingFeeController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class ShippingFeeController extends Controller
+{
+
+    /**
+     * getProvinces
+     *
+     * @return void
+     */
+    public function getProvinces()
+    {
+        
+        $response = Http::withHeaders([
+            'key' => env('RAJAONGKIRAPI')
+        ])->get('https://api.rajaongkir.com/starter/province');
+
+        $provinces = $response['rajaongkir']['results'];
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Get All Provinces',
+            'data'    => $provinces    
+        ]);
+    }
+    
+    /**
+     * getCities
+     *
+     * @param  mixed $id
+     * @return void
+     */
+    public function getCities($id)
+    {
+        
+        $response = Http::withHeaders([
+            'key' => env('RAJAONGKIRAPI')
+        ])->get('https://api.rajaongkir.com/starter/city?&province='.$id.'');
+
+        $cities = $response['rajaongkir']['results'];
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Get City By ID Provinces : '.$id,
+            'data'    => $cities    
+        ]);
+    }
+        
+    /**
+     * checkOngkir
+     *
+     * @param  mixed $request
+     * @return void
+     */
+    public function checkOngkir(Request $request)
+    {
+        $response = Http::withHeaders([
+            'key' => env('RAJAONGKIRAPI')
+        ])->post('https://api.rajaongkir.com/starter/cost', [
+            'origin'            => $request->origin,
+            'destination'       => $request->destination,
+            'weight'            => $request->weight,
+            'courier'           => $request->courier
+        ]);
+
+        $ongkir = $response['rajaongkir']['results'];
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Result Cost Ongkir',
+            'data'    => $ongkir    
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/provinces', [App\Http\Controllers\ShippingFeeController::class, 'getProvinces']);
+Route::get('/cities/{id}', [App\Http\Controllers\ShippingFeeController::class, 'getCities']);
+Route::post('/checkOngkir', [App\Http\Controllers\ShippingFeeController::class, 'checkOngkir']);
